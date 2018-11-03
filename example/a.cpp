1eebb2ce7b1d290058c7e3a41181c3a695fee84d
Caleb Zulawski <caleb.zulawski@gmail.com>
2018-11-03 01:50:01 +0000

    Control symbol visibility

--- example/a.cpp
+++ example/a.cpp
@@ -1,6 +1,6 @@
 #include "a.h"
 #include <iostream>
 
-void a::print(int i, std::string s) {
+__attribute__((visibility("default"))) void a::print(int i, std::string s) {
   std::cout << "Library A: " << i << " " << s << std::endl;
 }
--- example/b.cpp
+++ example/b.cpp
@@ -2,11 +2,11 @@
 #include "a.h"
 #include <iostream>
 
-void b::print(int i, std::string s) {
+__attribute__((visibility("default"))) void b::print(int i, std::string s) {
   std::cout << "Library B: " << i << " " << s << std::endl;
 }
 
-void b::a::print(int i, std::string s) {
+__attribute__((visibility("default"))) void b::a::print(int i, std::string s) {
   ::b::print(i, s);
   ::a::print(i, s);
 }
