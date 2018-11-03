8306d01096a53c45758f4cb2e44f953d00360275
Caleb Zulawski <caleb.zulawski@gmail.com>
2018-11-03 04:45:56 +0000

    Now creates hashes

--- symbol-slasher/slasher.cpp
+++ symbol-slasher/slasher.cpp
@@ -3,6 +3,8 @@ extern "C" {
 }
 
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 struct Bfd_exception : std::exception {
   Bfd_exception() : msg(bfd_errmsg(bfd_get_error())) {}
@@ -12,42 +14,50 @@ private:
   const char *msg = nullptr;
 };
 
-struct Object {
-  Object(std::string path) : descriptor(bfd_openr(path.c_str(), nullptr)) {
+struct Symbol_map {
+  void hash(const char *path) {
+    auto descriptor = bfd_openr(path, nullptr);
     if (descriptor == nullptr)
       throw Bfd_exception();
 
     if (!bfd_check_format(descriptor, bfd_object))
       throw Bfd_exception();
 
-    auto symbol_size = bfd_get_dynamic_symtab_upper_bound(descriptor);
-    if (symbol_size < 0)
+    auto symbols_size = bfd_get_dynamic_symtab_upper_bound(descriptor);
+    if (symbols_size < 0)
       throw Bfd_exception();
-    symbols = (bfd_symbol **)new char[symbol_size];
+    symbol_cache.resize(symbols_size);
 
+    auto symbols = reinterpret_cast<bfd_symbol **>(symbol_cache.data());
     auto symbol_count = bfd_canonicalize_dynamic_symtab(descriptor, symbols);
     if (symbol_count < 0)
       throw Bfd_exception();
 
     for (int i = 0; i < symbol_count; i++) {
       if (symbols[i]->value != 0)
-        std::cout << symbols[i]->name << std::endl;
+        symbol_hashes[symbols[i]->name] = symbol_hashes.size();
     }
   }
 
-  ~Object() {
-    bfd_close(descriptor);
-    delete symbols;
+  void print() {
+    for (auto symbol : symbol_hashes)
+      std::cout << symbol.second << " " << symbol.first << std::endl;
   }
 
 private:
-  bfd *descriptor;
-  bfd_symbol **symbols;
+  std::vector<uint8_t> symbol_cache;
+  std::unordered_map<std::string, uint64_t> symbol_hashes;
 };
 
-int slash(std::string path) try {
+int main(int argc, char **argv) try {
   bfd_init();
-  Object obj(path);
+  if (argc < 2)
+    return -1;
+
+  Symbol_map map;
+  for (int i = 1; i < argc; i++)
+    map.hash(argv[i]);
+  map.print();
   return 0;
 } catch (const Bfd_exception &e) {
   std::cerr << "libbfd error: " << e.what() << std::endl;
@@ -55,11 +65,4 @@ int slash(std::string path) try {
 } catch (const std::exception &e) {
   std::cerr << "exception: " << e.what() << std::endl;
   return -1;
-}
-
-int main(int argc, char **argv) {
-  if (argc != 2)
-    return -1;
-  std::string path = argv[1];
-  return slash(path);
 }
